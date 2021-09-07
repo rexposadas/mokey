74762c68256188e2fa9a16e78ef1fe6e559debbc
Rex Posadas <rexposadas@gmail.com>
2021-09-07 16:45:01 +0800

    passing tests, working repl, initial parser

---
+++ parser/let_test.go
@@ -0,0 +1,38 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/rexposadas/monkey/lexer"
+)
+
+func TestLet(t *testing.T) {
+	input := `
+		let x = 5;
+		let y = 10;
+		let foobar = 838383;
+`
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements))
+	}
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
