4baa441567be75c169a58bf509beaf1468539e2d
Rex Posadas <rexposadas@gmail.com>
2021-09-07 10:46:20 +0800

    added test for double characters

---
+++ lexer/doublekw_test.go
@@ -0,0 +1,42 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/rexposadas/monkey/token"
+)
+
+func TestDoubleKeyword(t *testing.T) {
+	input := `
+		10 == 10;
+		10 != 9;
+`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
