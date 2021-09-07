4666ea982961793be3ccaf172d093455f6b11909
Rex Posadas <rexposadas@gmail.com>
2021-09-07 09:53:26 +0800

    passing go test ./lexer

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	log.Printf("running")
+
+	file, err := os.Open("sample.yn")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+}
